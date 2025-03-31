ba5dcf6e272370caf13e6097a72c38fb0cc608d1
Gustavo <tomasgustavo99@gmail.com>
2025-03-30 23:19:50 -0300

    Change style to fit all data. Name, ID and pokemon img are now being called from API

--- script.js
+++ script.js
@@ -3,20 +3,35 @@ let url = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon"
 // Document elements
 const user_input = document.getElementById("search-input")
 const search_button = document.getElementById("search-button")
+const pokemon_name = document.getElementById("pokemon-name")
+const pokemon_id = document.getElementById("pokemon-id")
+const pokemon_img = document.getElementById("pokemon-img")
 
-
-
-const fetch_pokemon_api = async(pokemon) => {
-    try{
+const fetch_pokemon_api = async (pokemon) => {
+    try {
         const res = await fetch(url + "/" + pokemon)
         const data = await res.json()
+        console.log(url + "/" + pokemon)
         return data
     }
-    catch(err){
+    catch (err) {
         console.error(err)
         throw err
     }
 }
-fetch_pokemon_api(user_input).then((pokemon_data) => {
+fetch_pokemon_api(25).then((pokemon_data) => {
     console.log(pokemon_data)
+})
+
+search_button.addEventListener("click", (e) => {
+    e.preventDefault()
+    const user_pokemon = user_input.value
+
+    fetch_pokemon_api(user_pokemon).then((pokemon_data) => {
+        // Fill data
+        pokemon_name.innerText = pokemon_data.name[0].toUpperCase() + pokemon_data.name.slice(1,)
+        pokemon_id.innerText = `#${pokemon_data.id}`
+        pokemon_img.src = pokemon_data.sprites.front_default
+    })
+
 })
